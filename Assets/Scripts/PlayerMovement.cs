112068d2a1a37d17388cbd38b82a7e58feee41fb
Tanner Olsen <tanner-olsen@hotmail.com>
2023-02-03 21:15:37 -0500

    Added in player movement, tilesets, grid, and player rotation.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //AudioSource footsteps;
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+    void Start()
+    {
+        //footsteps = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+
+
+
+        //if (movement.x != 0 || movement.y != 0)
+        //{
+        //    footsteps.Play();
+        //}
+    }
+}
