d7d68d3b8ae6727209285df4661d68ef869cac80
Cole <coleportolesi@gmail.com>
2023-02-03 23:34:00 -0500

    Audio Manager, Imported audio files, Setup Sound and SFX sources

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+       if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+       else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //private void Start()
+    //{
+    //    PlayMusic("Menu Theme"); // Play Music on game start. Replace with background/level music when found/made
+    //    Debug.Log("Playing Menu Theme");
+    //}
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.name == name); // looking for sound file/check if file match
+
+        if (s == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+
+        
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name); // looking for sound file/check if file match
+
+        if (s == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+}
