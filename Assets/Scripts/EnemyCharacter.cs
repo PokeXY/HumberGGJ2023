d1d7922090f4f3947e0c3ca49697c3cb688b2e31
Tanner Olsen <tanner-olsen@hotmail.com>
2023-02-04 22:45:55 -0500

    Added in NavMeshPlus, Smart AI that circumvents obstacles, wolf sprite + animation, player death but no game over screen!

--- Assets/Scripts/EnemyCharacter.cs
+++ Assets/Scripts/EnemyCharacter.cs
@@ -1,30 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 /// <summary>
+/// 
 /// Distance and Player Tracking code courtesy of MoreBBlakeyyy
 /// https://www.youtube.com/watch?v=2SXa10ILJms
 /// January 16th, 2022
+///
 /// Indefinite attacking loop courtesy of Tanner Olsen
+/// 
+/// Credit to h8man for the NavMeshPlus system.
+/// https://github.com/h8man/NavMeshPlus
+/// 
 /// </summary>
 
 public class EnemyCharacter : MonoBehaviour
 {
-    public GameObject player;
-    public float speed;
+    [SerializeField] Transform player;
 
+    public float speed;
     public bool isAttacking;
+    public Animator animator;
+
     private float distance;
+    private NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        isAttacking = false;
     }
 
-    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            //SceneManager.LoadScene("Game Over");
+        }
+    }
+
+        // Update is called once per frame
     void Update()
     {
+
+        
         // Get the distance of our player from our enemy
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
@@ -47,8 +72,10 @@ public class EnemyCharacter : MonoBehaviour
         if (isAttacking == true)
         {
             // Move toward the target player Game Object
-            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position,
-            speed * Time.deltaTime);
+            //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position,
+            //speed * Time.deltaTime);
+            agent.SetDestination(player.position);
+            animator.SetFloat("Speed", 1);
         }
     }
 }
